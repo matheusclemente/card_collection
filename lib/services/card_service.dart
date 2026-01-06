ba13f5c35b036aba2269c90d4b33f35655b3c31c
Matheus Clemente <matheus_klementt@hotmail.com>
2026-01-05 21:32:54 -0300

    Add service class for card fetching

---
+++ lib/services/card_service.dart
@@ -0,0 +1,18 @@
+import '../models/collectible_card.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class CardService {
+  final String apiUrl = 'https://api.tcgdex.net/v2/en/cards';
+
+  Future<List<CollectibleCard>> fetchCards() async {
+    final response = await http.get(Uri.parse(apiUrl));
+
+    if (response.statusCode == 200) {
+      List<dynamic> jsonList = jsonDecode(response.body) as List<dynamic>;
+      return jsonList.map((json) => CollectibleCard.fromJson(json)).toList();
+    } else {
+      throw Exception('Failed to load cards');
+    }
+  }
+}
