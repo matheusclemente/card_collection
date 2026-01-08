ef6c46acd206612aa647718b8a955180089f9ca8
Matheus Clemente <matheus_klementt@hotmail.com>
2026-01-08 15:48:04 -0300

    Add services for fetching card sets

--- lib/services/card_service.dart
+++ lib/services/card_service.dart
@@ -1,13 +1,15 @@
+import 'package:card_collection/models/card_set.dart';
+
 import '../models/collectible_card.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class CardService {
-  final String apiUrl = 'https://api.tcgdex.net/v2/en/cards';
+  final String baseApiUrl = 'https://api.tcgdex.net/v2/en';
 
   Future<List<CollectibleCard>> fetchCards() async {
     final response = await http.get(Uri.parse(
-        '$apiUrl?sort:field=releaseDate&sort:order=ASC&pagination:page=1&pagination:itemsPerPage=60'));
+        '$baseApiUrl/cards?sort:field=set&sort:order=DSC&pagination:page=1&pagination:itemsPerPage=60'));
 
     if (response.statusCode == 200) {
       List<dynamic> jsonList = jsonDecode(response.body) as List<dynamic>;
@@ -18,13 +20,37 @@ class CardService {
   }
 
   Future<CollectibleCard> fetchCardById(String id) async {
-    final response = await http.get(Uri.parse('$apiUrl/$id'));
+    final response = await http.get(Uri.parse('$baseApiUrl/cards/$id'));
 
     if (response.statusCode == 200) {
       Map<String, dynamic> json = jsonDecode(response.body);
       return CollectibleCard.fromJson(json);
     } else {
       throw Exception('Failed to load card with id $id');
+    }
+  }
+
+  Future<List<CardSet>> fetchCardSets() async {
+    final response = await http.get(
+        Uri.parse('$baseApiUrl/sets?sort:order=DESC&sort:field=releaseDate'));
+
+    if (response.statusCode == 200) {
+      List<dynamic> jsonList = jsonDecode(response.body) as List<dynamic>;
+      return jsonList.map((json) => CardSet.fromJson(json)).toList();
+    } else {
+      throw Exception('Failed to load card sets');
+    }
+  }
+
+  Future<List<CollectibleCard>> fetchCardsBySet(String setId) async {
+    final response = await http.get(Uri.parse(
+        '$baseApiUrl/cards?set=$setId&pagination:page=1&pagination:itemsPerPage=60'));
+
+    if (response.statusCode == 200) {
+      List<dynamic> jsonList = jsonDecode(response.body) as List<dynamic>;
+      return jsonList.map((json) => CollectibleCard.fromJson(json)).toList();
+    } else {
+      throw Exception('Failed to load cards for set $setId');
     }
   }
 }
--- lib/viewmodels/cardlist_viewmodel.dart
+++ lib/viewmodels/cardlist_viewmodel.dart
@@ -1,14 +1,18 @@
+import 'package:card_collection/models/card_set.dart';
 import 'package:flutter/foundation.dart';
 import '../models/collectible_card.dart';
+import '../models/card_set.dart';
 import '../services/card_service.dart';
 
 class CardListViewModel extends ChangeNotifier {
   final CardService _cardService = CardService();
+  List<CardSet> _cardSets = [];
   List<CollectibleCard> _cards = [];
   bool _isLoading = false;
   String? _errorMessage;
 
   List<CollectibleCard> get cards => _cards;
+  List<CardSet> get cardSets => _cardSets;
   bool get isLoading => _isLoading;
   String? get errorMessage => _errorMessage;
 
@@ -23,6 +27,15 @@ class CardListViewModel extends ChangeNotifier {
       _errorMessage = e.toString();
     } finally {
       _isLoading = false;
+      notifyListeners();
+    }
+  }
+
+  Future<void> loadCardSets() async {
+    try {
+      _cardSets = await _cardService.fetchCardSets();
+    } catch (e) {
+      _errorMessage = e.toString();
       notifyListeners();
     }
   }
